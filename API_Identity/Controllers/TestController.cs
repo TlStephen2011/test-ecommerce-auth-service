348fcb6425be9289375e5e782e58f02d2b1a4fbb
tstephen <tlstephen11@gmail.com>
2025-03-02 15:53:56 +0200

    Test

---
+++ API_Identity/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Identity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : Controller
+{
+   [HttpGet]
+   public IActionResult Test()
+   {
+      return Ok("This is a test");
+   }
+}
